fe096690911212f80fa68af06a734522af5a4664
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-24 22:07:09 +0600

    Debad mode change

--- djangopollsite/settings.py
+++ djangopollsite/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 
 ALLOWED_HOSTS = ['gjangopollsite.herokuapp.com']
