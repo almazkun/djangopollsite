c90cea765db1e4e9652d1d5fed51ec11fe1305f8
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-25 14:26:04 +0600

    Heroku setting change

--- djangopollsite/settings.py
+++ djangopollsite/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 
-ALLOWED_HOSTS = ['gjangopollsite.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['gjangopollsite.herokuapp.com', '127.0.0.1']
 
 
 INSTALLED_APPS = [
