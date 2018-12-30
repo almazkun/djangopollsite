c9ffb3ef360b6271b3d05669386e11f5f8853c50
Almaz Kunpeissov <almaz.kun@gmail.com>
2018-12-30 21:12:07 +0600

    Adding urls and htmls

--- djangopollsite/settings.py
+++ djangopollsite/settings.py
@@ -8,7 +8,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 
 ALLOWED_HOSTS = ['gjangopollsite.herokuapp.com', '127.0.0.1']
